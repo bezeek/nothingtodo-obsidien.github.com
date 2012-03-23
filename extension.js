5e9554a6cd2532e6bcf6dd552fbea2082dfb0ff3
A. B <arnaud.bonatti@gmail.com>
2012-03-23 17:38:36 +0100

    ready for 3.4

--- extension.js
+++ extension.js
@@ -1,30 +1,20 @@
-const St = imports.gi.St;
-const Main = imports.ui.main;
+const St = imports.gi.St
+const Main = imports.ui.main
 
-function NothingToDo() {
-    this._init();
+function init() {}
+
+let items, index
+function enable() { 
+	let planning = Main.panel._dateMenu._eventList.actor.get_parent()
+	items = planning.get_parent().get_children()
+	index = items.indexOf(planning)
+	
+	items[index].hide()
+	items[(index == 0) ? index+1 : index-1].hide()
+	Main.panel._dateMenu.menu._arrowAlignment = 0.50
 }
-
-NothingToDo.prototype = {
-    _init: function() {
-        let planning = Main.panel._dateMenu._eventList.actor.get_parent();
-        this.items = planning.get_parent().get_children();
-        this.index = this.items.indexOf(planning);
-    },
-    
-    enable: function() {        
-        this.items[this.index].hide();
-        this.items[(this.index == 0) ? this.index+1 : this.index-1].hide();
-        Main.panel._dateMenu.menu._arrowAlignment=0.5;
-    },
-    
-    disable: function() {        
-        this.items[this.index].show();
-        this.items[(this.index == 0) ? this.index+1 : this.index-1].show();
-        Main.panel._dateMenu.menu._arrowAlignment=0.25;
-    }
-}
-
-function init() {
-    return new NothingToDo();
+function disable() {
+	items[index].show()
+	items[(index == 0) ? index+1 : index-1].show()
+	Main.panel._dateMenu.menu._arrowAlignment = 0.25
 }
